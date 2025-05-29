a827f1b0ed0d063efb21171c3b970e85a1176532
pragra <sunny@pragra.io>
2025-05-28 20:52:43 -0400

    added another sout in feature

--- Staging.java
+++ Staging.java
@@ -1,5 +1,6 @@
 public class Staging {
     public static void main(String[] args) {
         System.out.println("Added staging file...");
+        System.out.println("added feature branch");
     }
 }

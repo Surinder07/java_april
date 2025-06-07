edcc7eef9fb2fa1068325b04cec484b14045b9a6
pragra <sunny@pragra.io>
2025-06-07 14:35:59 -0400

    class typo

--- Test/Java.java
+++ Test/Java.java
@@ -1,7 +1,7 @@
 package Test;
 
 public class Java {
-    public static void main(String[] args) {
+    public static void main(String[] args)
         System.out.println("Added on June 7 2:30 pm");
 
 

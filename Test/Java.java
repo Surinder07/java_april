6a779d8bf652e248a68eb08cad24098f3592b74c
pragra <sunny@pragra.io>
2025-06-07 14:49:24 -0400

    added a new line

--- Test/Java.java
+++ Test/Java.java
@@ -3,5 +3,6 @@ package Test;
 public class Java {
     public static void main(String[] args){
         System.out.println("Added on June 7 2:30 pm");
+        System.out.println("Added a line in edward branch");
     }
 }

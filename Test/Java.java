a81acbf3ef56a2eced5e7a0479c133036c37ea55
pragra <sunny@pragra.io>
2025-06-07 14:59:41 -0400

    added new line

--- Test/Java.java
+++ Test/Java.java
@@ -2,6 +2,6 @@ package Test;
 
 public class Java {
     public static void main(String[] args){
-        System.out.println("Added on June 7 2:30 pm");
+        System.out.println("added by sunny at 3 pm ");
     }
 }
